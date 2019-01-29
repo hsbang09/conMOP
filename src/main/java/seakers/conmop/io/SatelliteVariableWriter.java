3855752d8a82115dcc46aa50331b6bcafdabac25
Bang <hb398@cornell.edu>
2019-01-29 00:17:57 -0500

    Fixes the OutOfMemoryError inside io function

--- src/main/java/seakers/conmop/io/SatelliteVariableWriter.java
+++ src/main/java/seakers/conmop/io/SatelliteVariableWriter.java
@@ -32,101 +32,115 @@ public class SatelliteVariableWriter {
 
         System.out.println("Saving file: " + filename);
 
-        StringJoiner header = new StringJoiner(",");
-        StringBuilder content = new StringBuilder();
-
-        ConstellationVariable temp = (ConstellationVariable) population.get(0).getVariable(0);
-
-        for(int i = 0; i < maxNumSat; i++){
-            header.add("sma" + i);
-        }
-        for(int i = 0; i < maxNumSat; i++){
-            header.add("inc" + i);
-        }
-        for(int i = 0; i < maxNumSat; i++){
-            header.add("raan" + i);
-        }
-        for(int i = 0; i < maxNumSat; i++){
-            header.add("ta" + i);
-        }
-
-        header.add("mean_resp");
-        header.add("num_sats");
-        header.add("avg_sma");
-
-        for(Solution solution: population){
-
-            double[] objectives = solution.getObjectives();
-            ConstellationVariable constel = (ConstellationVariable) solution.getVariable(0);
-            List<Double> sma = new ArrayList<>();
-            List<Double> inc = new ArrayList<>();
-            List<Double> raan = new ArrayList<>();
-            List<Double> ta = new ArrayList<>();
-
-            for (SatelliteVariable var : constel.getSatelliteVariables()) {
-                if (var instanceof BooleanSatelliteVariable) {
-                    if (!((BooleanSatelliteVariable) var).getManifest()) {
-                        continue;
-                    }
-                }
-                sma.add(var.getSma());
-                inc.add(var.getInc());
-                raan.add(var.getRaan());
-                ta.add(var.getTrueAnomaly());
-            }
-
-            StringJoiner row = new StringJoiner(",");
-
-            for(int i = 0; i < maxNumSat; i++){
-                if(i < sma.size()){
-                    row.add(Double.toString(sma.get(i)));
-                }else{
-                    row.add("null");
-                }
-            }
-
-            for(int i = 0; i < maxNumSat; i++){
-                if(i < inc.size()){
-                    row.add(Double.toString(inc.get(i)));
-                }else{
-                    row.add("null");
-                }
-            }
-
-            for(int i = 0; i < maxNumSat; i++){
-                if(i < raan.size()){
-                    row.add(Double.toString(raan.get(i)));
-                }else{
-                    row.add("null");
-                }
-            }
-
-            for(int i = 0; i < maxNumSat; i++){
-                if(i < ta.size()){
-                    row.add(Double.toString(ta.get(i)));
-                }else{
-                    row.add("null");
-                }
-            }
-
-            // Add objectives
-            for(double obj: objectives){
-                row.add(Double.toString(obj));
-            }
-
-            content.append(row.toString());
-            content.append("\n");
-        }
-
         File saveFile = new File(filename);
         saveFile.getParentFile().mkdirs();
 
         try (FileWriter writer = new FileWriter(saveFile)) {
 
+            StringJoiner header = new StringJoiner(",");
+
+            ConstellationVariable temp = (ConstellationVariable) population.get(0).getVariable(0);
+
+            for(int i = 1; i < maxNumSat + 1; i++){
+                header.add("sma" + i);
+            }
+            for(int i = 1; i < maxNumSat + 1; i++){
+                header.add("inc" + i);
+            }
+            for(int i = 1; i < maxNumSat + 1; i++){
+                header.add("raan" + i);
+            }
+            for(int i = 1; i < maxNumSat + 1; i++){
+                header.add("ta" + i);
+            }
+
+            header.add("mean_resp");
+            header.add("num_sats");
+            header.add("avg_sma");
+
             writer.append(header.toString());
             writer.append("\n");
-            writer.append(content.toString());
             writer.flush();
+
+            StringBuilder content = new StringBuilder();
+
+            int cnt = 0;
+            for(Solution solution: population){
+
+                double[] objectives = solution.getObjectives();
+                ConstellationVariable constel = (ConstellationVariable) solution.getVariable(0);
+                List<Double> sma = new ArrayList<>();
+                List<Double> inc = new ArrayList<>();
+                List<Double> raan = new ArrayList<>();
+                List<Double> ta = new ArrayList<>();
+
+                for (SatelliteVariable var : constel.getSatelliteVariables()) {
+                    if (var instanceof BooleanSatelliteVariable) {
+                        if (!((BooleanSatelliteVariable) var).getManifest()) {
+                            continue;
+                        }
+                    }
+                    sma.add(var.getSma());
+                    inc.add(var.getInc());
+                    raan.add(var.getRaan());
+                    ta.add(var.getTrueAnomaly());
+                }
+
+                StringJoiner row = new StringJoiner(",");
+
+                for(int i = 0; i < maxNumSat; i++){
+                    if(i < sma.size()){
+                        row.add(Double.toString(sma.get(i)));
+                    }else{
+                        row.add("null");
+                    }
+                }
+
+                for(int i = 0; i < maxNumSat; i++){
+                    if(i < inc.size()){
+                        row.add(Double.toString(inc.get(i)));
+                    }else{
+                        row.add("null");
+                    }
+                }
+
+                for(int i = 0; i < maxNumSat; i++){
+                    if(i < raan.size()){
+                        row.add(Double.toString(raan.get(i)));
+                    }else{
+                        row.add("null");
+                    }
+                }
+
+                for(int i = 0; i < maxNumSat; i++){
+                    if(i < ta.size()){
+                        row.add(Double.toString(ta.get(i)));
+                    }else{
+                        row.add("null");
+                    }
+                }
+
+                // Add objectives
+                for(double obj: objectives){
+                    row.add(Double.toString(obj));
+                }
+
+                content.append(row.toString());
+                content.append("\n");
+                cnt++;
+
+                if(cnt % 50 == 0){
+                    writer.append(content.toString());
+                    writer.flush();
+                    content = new StringBuilder();
+                    cnt = 0;
+                }
+            }
+
+            if(content.length() != 0){
+                writer.append(content.toString());
+                writer.flush();
+            }
 
         } catch (IOException e) {
             e.printStackTrace();
